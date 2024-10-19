f011b0131aff3b2e5fe57f3c658eed8fc13d6d1c
Yuri Pierri <yuripierriv@gmail.com>
2024-10-18 23:26:19 -0300

    final

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { XYPlot, MarkSeries, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, Crosshair } from 'react-vis';
 import 'react-vis/dist/style.css'; // Importe os estilos
 import axios from 'axios';
+import Alert from './Alert'; // Importa o novo componente de alerta
 
 function App() {
   const [processes, setProcesses] = useState([]);
@@ -38,7 +39,7 @@ function App() {
     setAlert({ message, type });
     setTimeout(() => {
       setAlert({ message: '', type: '' }); // Limpa a mensagem após 3 segundos
-    }, 1000);
+    }, 3000);
   };
 
   const fetchProcesses = async () => {
@@ -110,189 +111,192 @@ function App() {
   const uniqueLogicalClocks = [...new Set(data.map(event => event.x))].sort((a, b) => a - b);
 
   return (
-    <main className='container mt-5' >
-      {alert.message && (
-        <div className={`alert alert-${alert.type} alert-dismissible fade show alert-position w-50`} role="alert">
-          {alert.message}
-          <button type="button" className="btn-close" onClick={() => setAlert({ message: '', type: '' })} aria-label="Close"></button>
-        </div>
-      )}
-      <h1 className="text-center mb-4">Visualização de Processos</h1>
+    <div className='h-full w-full'>
+      <Alert
+        message={alert.message}
+        type={alert.type}
+        onClose={() => setAlert({ message: '', type: '' })} // Passa a função de fechar
+      />
+      <main className='container mt-5' >
+
+        <h1 className="text-center mb-4">Relógio de Lamport</h1>
 
 
 
-      <div className="row">
-        <div className="col-md-6">
-          <div className="card mb-4 shadow-sm">
-            <div className="card-body">
-              <h3 className="card-title text-center">Criar Novo Processo</h3>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="formProcessName" className="form-label">Nome do Processo</label>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card mb-4 shadow-sm">
+              <div className="card-body">
+                <h3 className="card-title text-center">Criar Novo Processo</h3>
+                <form>
+                  <div className="mb-3">
+                    <label htmlFor="formProcessName" className="form-label">Nome do Processo</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="formProcessName"
+                      value={newProcessName}
+                      onChange={(e) => setNewProcessName(e.target.value)}
+                      placeholder="Digite o nome do processo"
+                    />
+                  </div>
+                  <button type="button" className="btn btn-primary w-100" onClick={createProcess}>
+                    Criar Processo
+                  </button>
+                </form>
+              </div>
+            </div>
+
+            <div className="card mb-4 shadow-sm">
+              <div className="card-body">
+                <h3 className="card-title text-center">Enviar Mensagem entre Processos</h3>
+                <div className='mb-3'>
+                  <label className='form-label'>Processo Remetente</label>
+                  <select
+                    className='form-select'
+                    value={selectedSourceProcess}
+                    onChange={(e) => setSelectedSourceProcess(e.target.value)}
+                  >
+                    <option value=''>Selecione o processo remetente</option>
+                    {processes.map((process) => (
+                      <option key={process.id} value={process.id}>
+                        {process.name} (ID: {process.id})
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className='mb-3'>
+                  <label className='form-label'>Processo Destinatário</label>
+                  <select
+                    className='form-select'
+                    value={selectedDestinationProcess}
+                    onChange={(e) => setSelectedDestinationProcess(e.target.value)}
+                  >
+                    <option value=''>Selecione o processo destinatário</option>
+                    {processes.map((process) => (
+                      <option key={process.id} value={process.id}>
+                        {process.name} (ID: {process.id})
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className='mb-3'>
+                  <label className='form-label'>Mensagem</label>
                   <input
                     type="text"
                     className="form-control"
-                    id="formProcessName"
-                    value={newProcessName}
-                    onChange={(e) => setNewProcessName(e.target.value)}
-                    placeholder="Digite o nome do processo"
+                    value={messageContent}
+                    onChange={(e) => setMessageContent(e.target.value)}
+                    placeholder="Digite a mensagem"
                   />
                 </div>
-                <button type="button" className="btn btn-primary w-100" onClick={createProcess}>
-                  Criar Processo
+
+                <button className="btn btn-success w-100" onClick={sendMessage}>
+                  Enviar Mensagem
                 </button>
-              </form>
+              </div>
             </div>
           </div>
 
-          <div className="card mb-4 shadow-sm">
-            <div className="card-body">
-              <h3 className="card-title text-center">Enviar Mensagem entre Processos</h3>
-              <div className='mb-3'>
-                <label className='form-label'>Processo Remetente</label>
-                <select
-                  className='form-select'
-                  value={selectedSourceProcess}
-                  onChange={(e) => setSelectedSourceProcess(e.target.value)}
-                >
-                  <option value=''>Selecione o processo remetente</option>
-                  {processes.map((process) => (
-                    <option key={process.id} value={process.id}>
-                      {process.name} (ID: {process.id})
-                    </option>
-                  ))}
-                </select>
-              </div>
+          <div className='col-md-6'>
+            <div className="card shadow-sm mb-4">
+              <div className="card-body">
 
-              <div className='mb-3'>
-                <label className='form-label'>Processo Destinatário</label>
-                <select
-                  className='form-select'
-                  value={selectedDestinationProcess}
-                  onChange={(e) => setSelectedDestinationProcess(e.target.value)}
-                >
-                  <option value=''>Selecione o processo destinatário</option>
-                  {processes.map((process) => (
-                    <option key={process.id} value={process.id}>
-                      {process.name} (ID: {process.id})
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              <div className='mb-3'>
-                <label className='form-label'>Mensagem</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={messageContent}
-                  onChange={(e) => setMessageContent(e.target.value)}
-                  placeholder="Digite a mensagem"
-                />
-              </div>
-
-              <button className="btn btn-success w-100" onClick={sendMessage}>
-                Enviar Mensagem
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className='col-md-6'>
-          <div className="card shadow-sm">
-            <div className="card-body">
-
-              <table className='table table-hover'>
-                <thead className="table-dark">
-                  <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Relógio Lógico</th>
-                    <th>Ações</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {processes.map((process) => (
-                    <tr key={process.id}>
-                      <td>{process.id}</td>
-                      <td>{process.name}</td>
-                      <td>{process.logical_clock}</td>
-                      <td>
-                        <button
-                          className="btn btn-primary btn-sm me-2"
-                          onClick={() => createEvent(process.id)}
-                        >
-                          Criar Evento
-                        </button>
-                        <button
-                          className="btn btn-secondary btn-sm me-2"
-                          onClick={() => setSelectedSourceProcess(process.id)}
-                        >
-                          Remetente
-                        </button>
-                        <button
-                          className="btn btn-secondary btn-sm"
-                          onClick={() => setSelectedDestinationProcess(process.id)}
-                        >
-                          Destinatário
-                        </button>
-                      </td>
+                <table className='table table-hover'>
+                  <thead className="table-dark">
+                    <tr>
+                      <th>ID</th>
+                      <th>Nome</th>
+                      <th>Relógio Lógico</th>
+                      <th>Ações</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {processes.map((process) => (
+                      <tr key={process.id}>
+                        <td>{process.id}</td>
+                        <td>{process.name}</td>
+                        <td>{process.logical_clock}</td>
+                        <td>
+                          <button
+                            className="btn btn-primary btn-sm me-2"
+                            onClick={() => createEvent(process.id)}
+                          >
+                            Criar Evento
+                          </button>
+                          <button
+                            className="btn btn-secondary btn-sm me-2"
+                            onClick={() => setSelectedSourceProcess(process.id)}
+                          >
+                            Remetente
+                          </button>
+                          <button
+                            className="btn btn-secondary btn-sm"
+                            onClick={() => setSelectedDestinationProcess(process.id)}
+                          >
+                            Destinatário
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
-          </div>
-          <div className="card shadow-sm">
-            <div className="card-body">
+            <div className="card shadow-sm">
+              <div className="card-body">
 
-              <XYPlot
-                width={600}
-                height={400}
-                onMouseLeave={() => setCrosshair({})}
-                yDomain={[0, maxProcessId + 1]} // Define o limite do eixo Y como o maior ID + 1
-                xDomain={[0, Math.max(...events.map(event => event.logical_clock), 0) + 1]} // Define o limite do eixo X
-              >
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis
-                  title="Relógio Lógico"
-                  tickValues={uniqueLogicalClocks} // Define os ticks do eixo X apenas para os relógios lógicos únicos
-                />
-                <YAxis
-                  title="ID do Processo"
-                  tickValues={uniqueProcessIds} // Define os ticks do eixo Y apenas para os IDs únicos dos processos
-                />
-                <MarkSeries
-                  data={data} // Usando MarkSeries para mostrar pontos discretos
-                  onNearestX={value => setCrosshair({ x: value.x, y: value.y })}
-                />
-                <Crosshair values={[crosshair]}>
-                  <div
-                    className='container bg-light border rounded p-2 text-dark w-100'
-                    style={{
-                      width: '200px', // Largura fixa
-                      whiteSpace: 'nowrap', // Impede quebras de linha
-                      overflow: 'hidden', // Esconde texto que ultrapassa a largura
-                      textOverflow: 'ellipsis' // Adiciona '...' no final se o texto for muito longo
-                    }}
-                  >
-                    {crosshair.x !== undefined && (
-                      <div>
-                        <p>ID do Processo: {crosshair.y}</p>
-                        <p>
-                          Descrição: {data.find(d => d.x === crosshair.x && d.y === crosshair.y)?.description}
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                </Crosshair>
-              </XYPlot>
+                <XYPlot
+                  width={600}
+                  height={400}
+                  onMouseLeave={() => setCrosshair({})}
+                  yDomain={[0, maxProcessId + 1]} // Define o limite do eixo Y como o maior ID + 1
+                  xDomain={[0, Math.max(...events.map(event => event.logical_clock), 0) + 1]} // Define o limite do eixo X
+                >
+                  <VerticalGridLines />
+                  <HorizontalGridLines />
+                  <XAxis
+                    title="Relógio Lógico"
+                    tickValues={uniqueLogicalClocks} // Define os ticks do eixo X apenas para os relógios lógicos únicos
+                  />
+                  <YAxis
+                    title="ID do Processo"
+                    tickValues={uniqueProcessIds} // Define os ticks do eixo Y apenas para os IDs únicos dos processos
+                  />
+                  <MarkSeries
+                    data={data} // Usando MarkSeries para mostrar pontos discretos
+                    onNearestX={value => setCrosshair({ x: value.x, y: value.y })}
+                  />
+                  <Crosshair values={[crosshair]}>
+                    <div
+                      className='container bg-light border rounded p-2 text-dark w-100'
+                      style={{
+                        width: '200px', // Largura fixa
+                        whiteSpace: 'nowrap', // Impede quebras de linha
+                        overflow: 'hidden', // Esconde texto que ultrapassa a largura
+                        textOverflow: 'ellipsis' // Adiciona '...' no final se o texto for muito longo
+                      }}
+                    >
+                      {crosshair.x !== undefined && (
+                        <div>
+                          <p>ID do Processo: {crosshair.y}</p>
+                          <p>
+                            {data.find(d => d.x === crosshair.x && d.y === crosshair.y)?.description}
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                  </Crosshair>
+                </XYPlot>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
+
   );
 }
 
